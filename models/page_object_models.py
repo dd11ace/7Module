4aa2987d7f707cee2378c68b54f618acb4ee8a96
Gl <Dimadd11ace@yandex.ru>
2026-02-17 09:44:50 +0500

    Refactor

--- models/page_object_models.py
+++ models/page_object_models.py
@@ -6,17 +6,15 @@ from constants import (
     CINESCOPE_MOVIES_LINK,
     CINESCOPE_REGISTER_LINK,
 )
+from models.base_page import BasePage
 
 
-class CinescopeRegisterPage:
+class CinescopeRegisterPage(BasePage):
     """Класс для работы со страницей регистрации"""
 
     def __init__(self, page: Page):
-        self.page = page
+        super().__init__(page)
         self.url = CINESCOPE_REGISTER_LINK
-
-        self.home_button = page.get_by_role("link", name="Cinescope")
-        self.all_movies_button = page.get_by_role("link", name="Все фильмы")
 
         self.full_name_input = page.get_by_role("textbox", name="Имя Фамилия Отчество")
         self.email_input = page.get_by_role("textbox", name="Email")
@@ -29,75 +27,34 @@ class CinescopeRegisterPage:
 
         self.sign_button = page.get_by_role("link", name="Войти")
 
-    def go_to_home_page(self):
-        """Переход на главную страницу."""
-        self.home_button.click()
-        self.page.wait_for_url(CINESCOPE_MAIN_LINK)
-
-    def go_to_all_movies(self):
-        """Переход на страницу 'Все фильмы'."""
-        self.all_movies_button.click()
-        self.page.wait_for_url(CINESCOPE_MOVIES_LINK)
-
     def open(self):
         """Переход на страницу регистрации."""
         self.page.goto(self.url)
 
-    def enter_full_name(self, full_name: str):
-        """Ввод full_name"""
-        self.full_name_input.fill(full_name)
-
-    def enter_email(self, email: str):
-        """Ввод email"""
-        self.email_input.fill(email)
-
-    def enter_password(self, password: str):
-        """Ввод пароля"""
-        self.password_input.fill(password)
-
-    def enter_repeat_password(self, password: str):
-        """Ввод подтверждения пароля"""
-        self.repeat_password_input.fill(password)
-
-    def click_register_button(self):
-        """Клик по кнопке регистрации"""
-        self.register_button.click()
-
-    # Дополнительные действия
     def register(
         self, full_name: str, email: str, password: str, confirm_password: str
     ):
         """Полный процесс регистрации."""
-        self.enter_full_name(full_name)
-        self.enter_email(email)
-        self.enter_password(password)
-        self.enter_repeat_password(confirm_password)
-        self.click_register_button()
+        self.enter_text_to_element(self.full_name_input, full_name)
+        self.enter_text_to_element(self.email_input, email)
+        self.enter_text_to_element(self.password_input, password)
+        self.enter_text_to_element(self.repeat_password_input, password)
 
-    def wait_redirect_to_login_page(self):
-        """Переход на страницу login."""
-        self.page.wait_for_url(CINESCOPE_LOGIN_LINK)
-        expect(self.page).to_have_url(CINESCOPE_LOGIN_LINK)
+        self.click_element(self.register_button)
 
-    def check_allert(self):
-        """Проверка всплывающего сообщения после редиректа"""
-        notification_locator = self.page.get_by_text("Подтвердите свою почту")
-        notification_locator.wait_for(state="visible")
+    def assert_was_redirect_to_login_page(self):
+        self.wait_redirect_for_url(CINESCOPE_LOGIN_LINK)
 
-        expect(notification_locator).to_be_visible()
-        notification_locator.wait_for(state="hidden")
-        expect(notification_locator).not_to_be_visible()
+    def assert_allert_was_pop_up(self):
+        self.check_pop_up_element_with_text("Подтвердите свою почту")
 
 
-class CinescopeLoginPage:
+class CinescopeLoginPage(BasePage):
     """Класс для работы со страницей логина"""
 
     def __init__(self, page: Page):
-        self.page = page
+        super().__init__(page)
         self.url = CINESCOPE_LOGIN_LINK
-
-        self.home_button = page.get_by_role("link", name="Cinescope")
-        self.all_movies_button = page.get_by_role("link", name="Все фильмы")
 
         self.email_input = page.get_by_role("textbox", name="Email")
         self.password_input = page.get_by_role("textbox", name="Пароль")
@@ -105,48 +62,18 @@ class CinescopeLoginPage:
         self.login_button = "xpath=/html/body/div[2]/div/div/form/div[2]/button"
         self.register_link = page.get_by_role("link", name="Зарегистрироваться")
 
-    def go_to_home_page(self):
-        """Переход на главную страницу."""
-        self.home_button.click()
-        self.page.wait_for_url(CINESCOPE_MAIN_LINK)
-
-    def go_to_all_movies(self):
-        """Переход на страницу 'Все фильмы'."""
-        self.all_movies_button.click()
-        self.page.wait_for_url(CINESCOPE_MOVIES_LINK)
-
     def open(self):
         """Переход на страницу регистрации."""
         self.page.goto(self.url)
 
-    def enter_email(self, email: str):
-        """Ввод email"""
-        self.email_input.fill(email)
-
-    def enter_password(self, password: str):
-        """Ввод пароля"""
-        self.password_input.fill(password)
-
-    def click_login_button(self):
-        """Клик по кнопке входа"""
-        self.page.click(self.login_button)
-
-    # Дополнительные действия
     def login(self, email: str, password: str):
         """Полный процесс входа"""
-        self.enter_email(email)
-        self.enter_password(password)
-        self.click_login_button()
+        self.enter_text_to_element(self.email_input, email)
+        self.enter_text_to_element(self.password_input, password)
+        self.click_element(self.login_button)
 
-    def wait_redirect_to_home_page(self):
-        """Ожидание перехода на домашнюю страницу"""
-        self.page.wait_for_url(CINESCOPE_MAIN_LINK)
-        expect(self.page).to_have_url(CINESCOPE_MAIN_LINK)
+    def assert_was_redirect_to_home_page(self):
+        self.wait_redirect_for_url(self.home_url)
 
-    def check_allert(self):
-        """Проверка всплывающего сообщения после редиректа"""
-        notification_locator = self.page.get_by_text("Вы вошли в аккаунт")
-
-        expect(notification_locator).to_be_visible()
-        notification_locator.wait_for(state="hidden")
-        expect(notification_locator).not_to_be_visible()
+    def assert_allert_was_pop_up(self):
+        self.check_pop_up_element_with_text("Вы вошли в аккаунт")
