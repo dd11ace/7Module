8549acab71128ff2f60cd7b67b20e5d4371eebc2
Gl <Dimadd11ace@yandex.ru>
2026-02-17 09:54:31 +0500

    Added allure and refactor

--- ui/tests/test_registration_page.py
+++ ui/tests/test_registration_page.py
@@ -1,25 +1,41 @@
 import pytest
+import allure
 import time
 from playwright.sync_api import Page
 
 from models.page_object_models import CinescopeRegisterPage
 
 
+@allure.epic("Тестирование UI")
+@allure.feature("Тестирование страницы Register")
+@allure.label("qa_name", "Ivan Petrovich")
+@allure.tag("ui")
+@pytest.mark.ui
 class TestRegistrationPage:
     """Класс с тестами регистрации"""
 
+    @allure.feature("Регистрация")
+    @allure.story("Успешная регистрация")
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.title("Проведение успешной регистрации")
+    @pytest.mark.critical
+    @pytest.mark.smoke
+    @pytest.mark.regression
     def test_register_by_ui(
         self, page: Page, test_email: str, test_name: str, test_password: str
     ):
         random_password = test_password
         register_page = CinescopeRegisterPage(page)
         register_page.open()
-
-        register_page.register(
-            f"PlaywrightTest {test_name}", test_email, random_password, random_password
-        )
-
-        register_page.wait_redirect_to_login_page()
-        register_page.check_allert()
-
+        with allure.step("Заполнение полей"):
+            register_page.register(
+                f"PlaywrightTest {test_name}",
+                test_email,
+                random_password,
+                random_password,
+            )
+        with allure.step("Валидация результата"):
+            register_page.assert_was_redirect_to_login_page()
+            register_page.make_screenshot_and_attach_to_allure()
+            register_page.assert_allert_was_pop_up()
         time.sleep(3)
